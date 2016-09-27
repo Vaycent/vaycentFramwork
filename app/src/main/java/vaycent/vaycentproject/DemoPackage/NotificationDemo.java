d30634e8d721307c7c4b87ea598c97853b1104ec
Vaycent <vaycentsun@gmail.com>
2016-09-27 18:40:05 +0800

    add no kill notification

--- app/src/main/java/vaycent/vaycentproject/DemoPackage/NotificationDemo.java
+++ app/src/main/java/vaycent/vaycentproject/DemoPackage/NotificationDemo.java
@@ -2,20 +2,23 @@ package vaycent.vaycentproject.DemoPackage;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import vaycent.vaycentproject.MainActivity;
 import vaycent.vaycentproject.R;
 
 /**
  * Created by Vaycent on 16/9/9.
  */
 public class NotificationDemo extends AppCompatActivity {
-    private Button startSmallNotificationBtn,startLargeNotificationBtn;
+    private Button startSmallNotificationBtn,startLargeNotificationBtn,startNoKillNotificationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,7 @@ public class NotificationDemo extends AppCompatActivity {
 
         startSmallNotificationBtn=(Button)findViewById(R.id.startSmallNotificationBtn);
         startLargeNotificationBtn=(Button)findViewById(R.id.startLargeNotificationBtn);
+        startNoKillNotificationBtn=(Button)findViewById(R.id.startNoKillNotificationBtn);
 
         startSmallNotificationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +40,13 @@ public class NotificationDemo extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 pushLargeLocalNotification();
+            }
+        });
+
+        startNoKillNotificationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pushCanNotKillNotification();
             }
         });
     }
@@ -53,7 +64,7 @@ public class NotificationDemo extends AppCompatActivity {
                 .setDefaults(Notification.DEFAULT_SOUND)
                 .build();
 
-        nm.notify("test",0,updateNotification);
+        nm.notify("smallTest",0,updateNotification);
     }
 
     private void pushLargeLocalNotification(){
@@ -69,6 +80,41 @@ public class NotificationDemo extends AppCompatActivity {
                 .setDefaults(Notification.DEFAULT_SOUND)
                 .build();
 
-        nm.notify("test",0,updateNotification);
+        nm.notify("largeTest",1,updateNotification);
     }
+
+    private void pushCanNotKillNotification(){
+
+        NotificationManager nm=(NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        Intent notificationIntent = new Intent();
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+        notificationIntent.setClass(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        // notification.setLatestEventInfo(this,"Play Video Service","",
+        // pendingIntent);
+
+        Notification.Builder builder = new Notification.Builder(this);
+
+        builder.setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_face_black_24dp)
+                // 　　　　　　　　　　　　　　　　　　　　.setLargeIcon(BitmapFactory.decodeResource(res,
+                // R.drawable.i5))//下拉下拉列表里面的图标（大图标）
+                // 　　　　　　　.setTicker("this is bitch!")
+                // //设置状态栏的显示的信息
+                .setWhen(System.currentTimeMillis())// 设置时间发生时间
+                .setAutoCancel(true)// 设置可以清除
+                .setContentTitle("Send Sms")// 设置下拉列表里的标题
+                .setContentText("Keep alive services");// 设置上下文内容
+        Notification updateNotification = builder.getNotification();
+
+//        // using id of ticker text as notif id
+//        this.startForeground(R.string.app_name, notification);
+
+        nm.notify("largeTest",1,updateNotification);
+    }
+
+
 }

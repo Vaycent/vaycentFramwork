db7c18fb9735ce114bd00c4d8e25767a5d0863e5
Vaycent <vaycentsun@gmail.com>
2016-12-05 18:09:56 +0800

    sync web view demo

--- app/src/main/java/vaycent/vaycentproject/DemoPackage/WebPackage/WebViewDemo.java
+++ app/src/main/java/vaycent/vaycentproject/DemoPackage/WebPackage/WebViewDemo.java
@@ -6,6 +6,8 @@ import android.webkit.WebView;
 
 import vaycent.vaycentproject.R;
 
+import static android.webkit.WebSettings.LOAD_NO_CACHE;
+
 /**
  * Created by Vaycent on 2016/12/5.
  */
@@ -26,8 +28,10 @@ public class WebViewDemo extends AppCompatActivity {
 
     private void InitMyWebView(){
         myWebView.getSettings().setJavaScriptEnabled(true);//支持JavaScript
-        myWebView.getSettings().setUseWideViewPort(true);
-        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.getSettings().setUseWideViewPort(true);//设置载入页面自适应手机屏幕，居中显示
+        myWebView.getSettings().setLoadWithOverviewMode(true);//设置载入页面自适应手机屏幕，居中显示
+        myWebView.getSettings().setBuiltInZoomControls(true);//支持缩放
+        myWebView.getSettings().setCacheMode(LOAD_NO_CACHE);//设置缓存策略
 
         myWebView.loadUrl("https://www.baidu.com/");
     }

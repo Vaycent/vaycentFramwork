9617fccf581e37a927b1a1a3ad072fb7123f3b59
Vaycent <vaycentsun@gmail.com>
2016-12-06 17:58:51 +0800

    Sync webview demo

--- app/src/main/java/vaycent/vaycentproject/DemoPackage/WebPackage/WebViewDemo.java
+++ app/src/main/java/vaycent/vaycentproject/DemoPackage/WebPackage/WebViewDemo.java
@@ -1,12 +1,23 @@
 package vaycent.vaycentproject.DemoPackage.WebPackage;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.appcompat.BuildConfig;
+import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import vaycent.magicLog.mlog;
 import vaycent.vaycentproject.R;
 
-import static android.webkit.WebSettings.LOAD_NO_CACHE;
+import static android.webkit.WebSettings.LOAD_DEFAULT;
 
 /**
  * Created by Vaycent on 2016/12/5.
@@ -24,15 +35,134 @@ public class WebViewDemo extends AppCompatActivity {
         myWebView = (WebView)findViewById(R.id.id_webview);
 
         InitMyWebView();
+
+        WebViewClientControl();
+
+        WebChromeClientControl();
+
+        //开通webview的debu模式
+        if (BuildConfig.DEBUG && Build.VERSION.SDK_INT
+                >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        resumeWebView();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        pauseWebView();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        destroyWebView();
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(myWebView.canGoBack()){
+//            myWebView.goBack();
+            myWebView.loadUrl("javascript:window.history.back();");//这种back的方式可以避免重定向
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+
 
     private void InitMyWebView(){
         myWebView.getSettings().setJavaScriptEnabled(true);//支持JavaScript
         myWebView.getSettings().setUseWideViewPort(true);//设置载入页面自适应手机屏幕，居中显示
         myWebView.getSettings().setLoadWithOverviewMode(true);//设置载入页面自适应手机屏幕，居中显示
+        myWebView.getSettings().setMediaPlaybackRequiresUserGesture(false);//支持media播放
         myWebView.getSettings().setBuiltInZoomControls(true);//支持缩放
-        myWebView.getSettings().setCacheMode(LOAD_NO_CACHE);//设置缓存策略
+        myWebView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);//BASE64 图片显示
 
-        myWebView.loadUrl("https://www.baidu.com/");
+        // 设置缓存策略 LOAD_DEFAULT, LOAD_CACHE_ELSE_NETWORK, LOAD_NO_CACHE, LOAD_CACHE_ONLY
+        myWebView.getSettings().setCacheMode(LOAD_DEFAULT);
+
+        myWebView.loadUrl("https://www.healthreach.hk/ehc/tablet/login.jsp?v=1&uid=04:EC:CE:2A:2C:26:80");
     }
+
+    private void WebViewClientControl(){
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String link) {
+                mlog.v("Catch this link:"+link);
+                myWebView.loadUrl(link);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap bitmap) {
+                super.onPageStarted(view, url, bitmap);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode,
+                                        String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler,
+                                           SslError error){
+                super.onReceivedSslError(view,handler,error);
+            }
+        });
+    }
+
+    private void WebChromeClientControl(){
+        myWebView.setWebChromeClient(new WebChromeClient()
+        {
+            // For Lollipop 5.0+ Devices
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams)
+            {
+                mlog.e("onShowFileChooser here");
+                return true;
+            }
+        });
+    }
+
+    //**************** Control The WebView Thread ****************
+    private void resumeWebView() {
+        if (myWebView == null) {
+            return;
+        }
+        myWebView.onResume();
+    }
+
+    private void pauseWebView() {
+        if (myWebView == null) {
+            return;
+        }
+        myWebView.onPause();
+    }
+
+    private void destroyWebView() {
+        if (myWebView != null) {
+            myWebView.removeAllViews();
+            myWebView.destroy();
+            myWebView = null;
+        }
+    }
+
+
+
+
+
 }
